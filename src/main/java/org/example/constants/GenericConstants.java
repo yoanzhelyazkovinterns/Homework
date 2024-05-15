465fb8d5528ab3a8853a1b831d19ef56e3050ff4
yoanzhelyazkov <yoan.zhelyazkov@expertallies.com>
2024-05-15 14:52:21 +0300

    Added implicit wait control

--- src/main/java/org/example/constants/GenericConstants.java
+++ src/main/java/org/example/constants/GenericConstants.java
@@ -4,4 +4,5 @@ public class GenericConstants {
 
     public static final String SEARCH_CRITERIA = "Harry Potter and the Cursed Child - Parts One and Two";
     public static final String BOOK_TYPE = "Paperback";
+    public static final int IMPLICIT_TIMEOUT = 10;
 }
--- src/main/java/org/example/utils/DriverProvider.java
+++ src/main/java/org/example/utils/DriverProvider.java
@@ -1,14 +1,20 @@
 package org.example.utils;
 
+import org.example.constants.GenericConstants;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.example.constants.GenericConstants.IMPLICIT_TIMEOUT;
 
 public class DriverProvider {
 
     public static WebDriver getDriver() {
         var driver = new ChromeDriver();
         driver.manage().window().maximize();
-
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT, TimeUnit.SECONDS);
+        
         return driver;
     }
 }
